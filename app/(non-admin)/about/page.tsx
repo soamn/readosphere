b11538391cd2fcb108b42017abf52120461d44bd
soamn <negiaman604@gmail.com>
2025-04-16 21:36:51 +0530

    about page and spacing fix

--- app/(non-admin)/about/page.tsx
+++ app/(non-admin)/about/page.tsx
@@ -2,35 +2,37 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <main
-      className="text-black text-base sm:text-lg md:text-xl lg:text-3xl
-       px-4 sm:px-6 md:px-10 lg:px-20 py-10 sm:py-16 md:py-20 lg:py-32
-       space-y-6 mt-20 lg:mt-48 bg-white max-w-5xl mx-auto "
-    >
-      <p>
-        Coming from the Himalayas this space is my little conscious effort not
-        to be swept away by the endless current of content online, I’ll admit:
-        there are still days when YouTube eats up more time than I care to
-        admit.
-      </p>
-      <p>
-        Somewhere between my work, some travels, and the quiet chaos of
-        adulting, I realize—this isn’t the life I want to look back on. I want
-        to grow old with a book in hand. And, reading has never just been about
-        escape; it’s been about return.
-      </p>
-      <p>
-        A return to self, to stillness, to wonder - and all this while living
-        under "a Cherry & Oak tree". Perks of living in the mountains. For
-        someone who believes in the healing power of calm, curiosity, and
-        slowing down—not quick fixes or 30-second distractions—{" "}
-        <b>this is about me!!</b>
-      </p>
-      <p>
-        A mindful corner filled with thoughtful reads and nature-inspired
-        reflections. Some for reading, some for imagining, some for learning —{" "}
-        <b>but mostly, for simply being.</b>
-      </p>
+    <main className="px-4 sm:px-6 md:px-10 lg:px-20 ">
+      <article
+        className="text-black text-base sm:text-lg md:text-xl lg:text-3xl
+       px-4 sm:px-6 md:px-1 lg:px-20 py-10 md:py-20 lg:py-32
+       space-y-6 mt-20 lg:mt-48 bg-white   "
+      >
+        <p>
+          Coming from the Himalayas this space is my little conscious effort not
+          to be swept away by the endless current of content online, I’ll admit:
+          there are still days when YouTube eats up more time than I care to
+          admit.
+        </p>
+        <p>
+          Somewhere between my work, some travels, and the quiet chaos of
+          adulting, I realize—this isn’t the life I want to look back on. I want
+          to grow old with a book in hand. And, reading has never just been
+          about escape; it’s been about return.
+        </p>
+        <p>
+          A return to self, to stillness, to wonder - and all this while living
+          under "a Cherry & Oak tree". Perks of living in the mountains. For
+          someone who believes in the healing power of calm, curiosity, and
+          slowing down—not quick fixes or 30-second distractions—{" "}
+          <b>this is about me!!</b>
+        </p>
+        <p>
+          A mindful corner filled with thoughtful reads and nature-inspired
+          reflections. Some for reading, some for imagining, some for learning —{" "}
+          <b>but mostly, for simply being.</b>
+        </p>
+      </article>
     </main>
   );
 }
--- app/(non-admin)/hero.tsx
+++ app/(non-admin)/hero.tsx
@@ -5,18 +5,16 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <section className="w-full h-[43rem] relative overflow-hidden ">
+      <section className="w-full h-[43rem] relative overflow-hidden group">
         <Image
           src="/hero1.jpg"
           alt="hero image"
           width={1920}
           height={1080}
-          className="lg:blur hover:blur-none w-full h-[43rem] object-cover select-none 
-         [mask-image:linear-gradient(to_bottom,_white,_transparent)]"
+          className="w-full h-[43rem] object-cover select-none group-hover:blur-[4px] transition-all duration-300 [mask-image:linear-gradient(to_bottom,_white,_transparent)]"
         />
-
         <div className="absolute w-full top-[40%] text-center cursor-pointer px-4">
-          <h1 className="flex flex-col  text-5xl md:text-6xl lg:text-8xl ">
+          <h1 className="flex flex-col text-5xl md:text-6xl lg:text-8xl group-hover:text-white transition-all duration-300">
             <span className="font-extralight selection:bg-black/30 tracking-widest">
               Rediscover the
             </span>
@@ -26,9 +24,10 @@ const Hero = () => {
           </h1>
         </div>
       </section>
+
       <section className="flex flex-col mb-20 md:px-20  text-[#FEFEFE] gap-y-7 lg:gap-0">
         {/* Heading: "Each word holds" */}
-        <h2 className="text-4xl md:6xl  lg:text-8xl  tracking-widest whitespace-nowrap relative md:-top-10 text-center   lg:text-left">
+        <h2 className="text-4xl md:6xl  lg:text-8xl  tracking-widest whitespace-nowrap  text-center   lg:text-left">
           Each word holds
         </h2>
 
